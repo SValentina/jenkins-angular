9102f0df77299f15c1fa3c8acec8b011d3d4fdf4
SValentina <39774399+SValentina@users.noreply.github.com>
2022-04-25 20:34:43 -0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -9,8 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require( 'karma-phantomjs-launcher'),
+      require('karma-coverage'),      
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -38,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS', 'Chrome'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
