6ae510cc86e15cc8a12f72c6a24dfbad078f67f4
SValentina <39774399+SValentina@users.noreply.github.com>
2022-04-28 23:16:19 -0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
       require('@angular-devkit/build-webpack/plugins/karma')
     ],
     client:{
