82c31d883e33b41babde5ff7aba477c9ca533b5e
SValentina <39774399+SValentina@users.noreply.github.com>
2022-04-28 23:33:55 -0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),      
-      require('@angular-devkit/build-webpack/plugins/karma')
+      require('@angular-devkit/build-webpack/plugins/karma'),
+      require('@angular/cli/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
